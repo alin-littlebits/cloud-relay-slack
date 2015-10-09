03ef922fa3996aef19e5655f05a2cdf34525a732
alin-littlebits <alin@littlebits.cc>
2015-10-09 15:32:20 -0400

    messin about

--- cmd/cloud-relay-slack/main.go
+++ cmd/cloud-relay-slack/main.go
@@ -18,5 +18,5 @@ func main() {
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "{\n\"text\": \"African or European?\"\n}")
+    fmt.Fprint(res, req.Body) //"{\n\"text\": \"African or European?\"\n}")
 }
