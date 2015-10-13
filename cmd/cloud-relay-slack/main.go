8b32f76b0306cd0cc2ca2e6475c86df4dae909c0
alin-littlebits <alin@littlebits.cc>
2015-10-13 15:28:08 -0400

    tudish 0

--- cmd/cloud-relay-slack/main.go
+++ cmd/cloud-relay-slack/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 	"log"
 	"io/ioutil"
@@ -11,9 +12,11 @@ import (
 
 var hc http.Client
 var api_call string = "https://api-http.littlebitscloud.cc/devices/243c200ccecb/output?access_token=97612f7c1ce1b4bd1e2c31707658ec1145a765abad9f1c4af67e6fb6267931c0"
+var go_call string = "https://hooks.slack.com/services/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"
 
 func main() {
     http.HandleFunc("/", hello)
+    http.HandleFun("/turd", chat)
 
     hc = http.Client{}
 
@@ -22,6 +25,23 @@ func main() {
     if err != nil {
       panic(err)
     }
+}
+
+func chat(res http.ResponseWriter, req *http.Request) {
+    req.ParseForm()
+    fmt.Printf("%s\n", req.Form)
+
+    resp, err := http.PostForm(go_call, url.Values{"payload": {"\{\"text\":\"\uD83D\uDCA9\"\}"}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req_res_body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", req_res_body)
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
